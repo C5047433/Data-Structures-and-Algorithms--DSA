dd07c99a60ea19bd95fc312e6bf8cc99604157f4
Vinod Dama <vindama@publicisgroupe.net>
2024-12-25 17:09:48 +0530

    Binary Search - Find Smallest Again

--- Binary Search/Binary Search Problems/Binary Search - Find Smallest Again.py
+++ Binary Search/Binary Search Problems/Binary Search - Find Smallest Again.py
@@ -6,26 +6,18 @@ If we store the sum of each triplet of the array A in a new list, then find the 
 
 NOTE: A triplet consists of three elements from the array. Let's say if A[i], A[j], A[k] are the elements of the triplet then i < j < k.
 
-
-
 Problem Constraints
 
 3 <= N <= 500
 1 <= A[i] <= 108
 1 <= B <= (N*(N-1)*(N-2))/6
 
-
-
 Input Format
 The first argument is an integer array A.
 The second argument is an integer B.
 
-
-
 Output Format
 Return an integer denoting the Bth element of the list.
-
-
 
 Example Input
 
@@ -57,7 +49,6 @@ Explanation 1:
  (4, 3, 2) = 9
 
  So the 3rd smallest element is 9.
-
 
 CODE:
 
