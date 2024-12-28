374ff3936c299516e3702a976fe50fe8c0e3cc1e
Vinod Dama <vinod.dama@gmail.com>
2024-12-28 13:23:28 +0530

    Binary Search - Agressive Cows

--- Binary Search/Binary Search-3-on Answer/Binary Search - Agressive Cows.py
+++ Binary Search/Binary Search-3-on Answer/Binary Search - Agressive Cows.py
@@ -4,8 +4,6 @@ Farmer John has built a new long barn with N stalls. Given an array of integers 
 
 His cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, John wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
 
-
-
 Problem Constraints
 2 <= N <= 100000
 0 <= A[i] <= 109
@@ -18,8 +16,6 @@ The second argument given is the integer B.
 Output Format
 Return the largest minimum distance possible among the cows.
 
-
-
 Example Input
 Input 1:
 A = [1, 2, 3, 4, 5]
@@ -29,7 +25,6 @@ Input 2:
 A = [1, 2]
 B = 2
 
-
 Example Output
 Output 1:
  2
@@ -37,13 +32,11 @@ Output 1:
 Output 2:
  1
 
-
 Example Explanation
-
 Explanation 1:
  John can assign the stalls at location 1, 3 and 5 to the 3 cows respectively. So the minimum distance will be 2.
-Explanation 2:
 
+Explanation 2:
 The minimum distance will be 1.
 
 
