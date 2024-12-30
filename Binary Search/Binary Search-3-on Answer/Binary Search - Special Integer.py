b3c06e8a8c4c6bab0aef97be005b934b118ae41e
Vinod Dama <vinod.dama@gmail.com>
2024-12-30 15:11:28 +0530

    Binary Search - Special Integer- space fomrats

--- Binary Search/Binary Search-3-on Answer/Binary Search - Special Integer.py
+++ Binary Search/Binary Search-3-on Answer/Binary Search - Special Integer.py
@@ -2,13 +2,10 @@ Problem Description
 
 Given an array of integers A and an integer B, find and return the maximum value K such that there is no subarray in A of size K with the sum of elements greater than B.
 
-
-
 Problem Constraints
 1 <= |A| <= 100000
 1 <= A[i] <= 10^9
 1 <= B <= 10^9
-
 
 Input Format
 The first argument given is the integer array A.
@@ -18,7 +15,6 @@ Output Format
 Return the maximum value of K (sub array length).
 
 Example Input
-
 Input 1:
 A = [1, 2, 3, 4, 5]
 B = 10
@@ -26,7 +22,6 @@ B = 10
 Input 2:
 A = [5, 17, 100, 11]
 B = 130
-
 
 Example Output
 Output 1:
