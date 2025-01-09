c169a3cf495016d83767bfb469ef785e299f574d
Vinod-Dama <vinod.dama@gmail.com>
2025-01-09 23:10:24 +0530

    Update Insert in Linked List.py

--- Linked List/Linked List- Basic Problems/Insert in Linked List.py
+++ Linked List/Linked List- Basic Problems/Insert in Linked List.py
@@ -5,23 +5,19 @@ In case the position is more than length of linked list, simply insert the new n
 In case the pos is 0, simply insert the new node at head only.
 Follow 0-based indexing for the node numbering.
 
-
 Problem Constraints
 0 <= size of linked list <= 105
 1 <= value of nodes <= 109
 1 <= B <= 109
 0 <= C <= 105
 
-
 Input Format
 The first argument A is the head of a linked list.
 The second argument B is an integer which denotes the value of the new node
 The third argument C is an integer which denotes the position of the new node
 
-
 Output Format
 Return the head of the linked list
-
 
 Example Input
 Input 1:
@@ -33,16 +29,27 @@ A = 1 -> 2
 B = 3
 C = 1
 
-
 Example Output
 Output 1:
 3 -> 1 -> 2
 Output 2:
 1 -> 3 -> 2
 
-
 Example Explanation
 For Input 1:
 The new node is add to the head of the linked list
 For Input 2:
 The new node is added after the first node of the linked list
+
+
+CODE:
+
+class Solution:
+  def insert_linkedlist(A, B, C):
+    current = A
+
+    if C == 0:
+      NewNode(3)
+    for i in range(B -1)):
+      while temp.next is not None:
+        tem= 
