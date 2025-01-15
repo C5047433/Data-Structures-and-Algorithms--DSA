48a344bc1bde68bdd66f470f89fa7f09a747d70f
Vinod-Dama <vinod.dama@gmail.com>
2025-01-15 20:20:35 +0530

    Custom Test inputs and Including function to print the function result

--- Linked List/Linked List- Basic Problems/K reverse linked list.py
+++ Linked List/Linked List- Basic Problems/K reverse linked list.py
@@ -47,19 +47,18 @@ Explanation 2:
 
 CODE:
 
-import sys
-sys.setrecursionlimit(10**6)
-
 class Node:
   def __init__(self, X):
     self.val = X
     self.next = None
 
+import sys
+sys.setrecursionlimit(10**6)
 
 class Solution:
   def k_reverse(self, A, K):
     prev = None
-    curernt = A
+    current = A
     nxt = None
     count = 0
 
@@ -69,10 +68,63 @@ class Solution:
       prev = current
       current = nxt
       count += 1
-    
-    if next != None:
-      A.next = self.k_reverse(A, K)
 
-    return Prev
+    if nxt != None:
+      A.next = self.k_reverse(nxt, K)
+
+    return prev
+
+'''
+To test this code in your own environment (e.g., Google Colab or standalone script), 
+uncomment(line 92 to line 130)-  the below helper functions and linked list setup code. Platforms like LeetCode 
+may already provide linked list creation and testing infrastructure.
+
+Follow these steps:
+1. Uncomment the `print_list` function and the linked list setup section below.
+2. Use this setup to test the `remove_duplicates` method in a local environment.
+3. Ensure that the `Solution` class and `Node` class are defined before running the test code.
+
+'''
+
+'''
+#print the updated list after reversing
+def print_list(A):
+  """
+  Traverses and prints the linked list.
+
+  Parameters:
+  A (Node): The head of the linked list.
+  """
+  current = A
+  while current is not None:
+    print(current.val, end="->")
+    current = current.next
+  print("None")
 
 
+#Creating Linked List input
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+node6 = Node(6)
+node7 = Node(7)
+
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+node5.next = node6
+node6.next = node7
+node7.next = None
+
+
+#create solution
+solution = Solution()
+new_head = solution.k_reverse(node1, K=5)
+
+#print output
+print_list(new_head)
+'''
