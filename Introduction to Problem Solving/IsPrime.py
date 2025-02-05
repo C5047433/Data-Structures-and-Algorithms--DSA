8a8157dc7f2b99e8de863ba693599b236d526b9d
Vinod-Dama <vinod.dama@gmail.com>
2025-02-05 20:40:17 +0530

    Update IsPrime.py

--- Introduction to Problem Solving/IsPrime.py
+++ Introduction to Problem Solving/IsPrime.py
@@ -45,19 +45,19 @@ class Solution:
     # @return an integer
     def solve(self, A):
         #1 is not a prime number so we intialize from 2
-        i=1
+        i = 1
         count = 0
       
         #we iterate till the squareroot of A
         while i * i <= A:
-            #check if the nuber is divisible by i or not, if yes increase the count by 1
+            #check if the number is divisible by i or not, if yes increase the count by 1
             if A%i == 0:
                 count += 1
                 
-                if i != A // i: # If it's a distinct factor, count it separately
+                if i != A // i: #If it's a distinct factor, count it separately
                     count += 1
                      
             i += 1 #keep increasing the value of i
         
-        # A prime number has exactly 2 factors (1 and itself)
+        #A prime number has exactly 2 factors (1 and itself)
         return 1 if count == 2 else 0
