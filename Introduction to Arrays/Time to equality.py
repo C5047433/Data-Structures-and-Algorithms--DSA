50079e4c160cc363aa3068f05216dc90929a93ec
Vinod-Dama <vinod.dama@gmail.com>
2025-02-24 18:48:55 +0530

    Arrays - Update Time to equality.py

--- Introduction to Arrays/Time to equality.py
+++ Introduction to Arrays/Time to equality.py
@@ -27,3 +27,31 @@ Example Output
 
 Example Explanation
 We can change the array A = [4, 4, 4, 4, 4]. The time required will be 8 seconds.
+
+
+======================================
+  CODE:
+======================================
+
+class Solution:
+    
+    def solve(self, A):
+      """Returns the minimum operations needed to make all elements equal to the max value."""
+      # @param A : list of integers
+      # @return an integer
+      
+      # Get Max value in the array
+      mx = A[0]
+
+      for i in range(1, len(A)):
+        
+        if mx < A[i]:
+          mx = A[i]
+
+      # Iterate over array
+      result = 0
+      for i in range(len(A)):
+        # Difference from max over each element is number of opearation needed for making values equal
+        result += mx - A[i]
+
+      return result 
