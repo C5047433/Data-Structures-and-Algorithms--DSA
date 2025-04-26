2edc349c7d14f372d5df7ab0b9bcf4a41744f424
Vinod-Dama <vinod.dama@gmail.com>
2025-04-26 23:19:24 +0530

    CODE Update Subarray with given sum and length.py

--- Arrays: Sliding Window & Contribution Technique/Subarray with given sum and length.py
+++ Arrays: Sliding Window & Contribution Technique/Subarray with given sum and length.py
@@ -42,3 +42,50 @@ Explanation 1:
 The subarray [3, 2, 6] is of length 3 and sum 11.
 Explanation 2:
 There are no such subarray.
+
+
+===============================================
+CODE:
+===============================================
+
+class Solution:
+    """
+    A : list of integers
+    B : integer (window size)
+    C : integer (target sum)
+    Returns 1 if a window of size B with sum C exists, else 0.
+    """
+    def solve(self, A, B, C):
+
+      n = len(A)
+      found = 0
+      if n == 1:
+        if A[0] == C:
+          found = 1
+      
+      else:
+        current_sum = 0
+        for i in range(B):
+          current_sum += A[i]
+
+          if current_sum == C:
+            found = 1
+
+        window_sum = current_sum
+
+        i = 0
+        j = B
+
+        while j < n:
+          window_sum -= A[i]
+          window_sum += A[j]
+
+          if window_sum == C:
+            found = 1
+          i += 1
+          j += 1
+      return found
+
+          
+
+        
