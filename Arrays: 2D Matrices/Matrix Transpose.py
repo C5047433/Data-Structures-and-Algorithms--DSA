387d0750f038c7a0b6f5e2f94b71645d0ea07cd5
Vinod-Dama <vinod.dama@gmail.com>
2025-05-15 22:42:54 +0530

    Code for Matrix Transpose.py

--- Arrays: 2D Matrices/Matrix Transpose.py
+++ Arrays: 2D Matrices/Matrix Transpose.py
@@ -38,3 +38,21 @@ Clearly after converting rows to column and columns to rows of [[1, 2, 3],[4, 5,
  we will get [[1, 4, 7], [2, 5, 8], [3, 6, 9]].
 Explanation 2:
 After transposing the matrix, A becomes [[1, 1, 1], [2, 2, 2]]
+
+
+=================================
+CODE:
+==================================
+class Solution:
+    # @param A : list of list of integers
+    # @return a list of list of integers
+    def solve(self, A):
+        #every element oafter transpose gets swapped i.e (i,j) becomes (j,i)
+        
+        mat=[[0]*len(A) for _ in range(len(A[0]))]
+        for i in range(len(A)):
+            #makesure to rotate colum from (i+1) to lenghtof columns i.e len(A[0])
+            #if we rotate every elemtn then it becomes origin  matrix element 
+            for j in range(len(A[0])):
+                mat[j][i]=A[i][j]
+        return mat
