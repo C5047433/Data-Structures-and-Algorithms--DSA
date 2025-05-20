b83294cf9473c92651b874f9aee980cd61a55e8a
Vinod-Dama <vinod.dama@gmail.com>
2025-05-20 21:12:08 +0530

    Code without XOR Toggle Case.py

--- DSA: Strings/Toggle Case.py
+++ DSA: Strings/Toggle Case.py
@@ -55,3 +55,19 @@ class Solution:
         for i in range(len(A)):
                 A[i] = chr(ord(A[i]) ^ 32)
         return "".join(A)
+
+
+======================================
+CODE without XOR:
+======================================
+class Solution:
+    # @param A : string
+    # @return a strings
+    def solve(self, A):
+        A = list(A)
+        for i in range(len(A)):
+            if ord(A[i]) >= 97 and ord(A[i]) <= 122:
+                A[i] = chr(ord(A[i]) - 32)
+            else:
+                A[i] = chr(ord(A[i]) + 32)
+        return "".join(A)
